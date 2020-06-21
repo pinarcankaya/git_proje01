71d91d302a1d7cbe949bd99deefcfe0000aca282
pinarcankaya <pinarunalcan50@gmail.com>
2020-06-21 21:33:25 +0200

    feature branch ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("ilk commit from local repository");
 		System.out.println("ikinci commit from local repository");
+		
+		System.out.println("yeni bir branch olusturdum adi feature");
 	}
 
 }
